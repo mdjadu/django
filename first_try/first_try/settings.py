862ba45485448e77e1b19d4027fada5bea19b66d
Krushna Patil <krushnap@cse.iitb.ac.in>
2020-05-25 02:42:10 +0530

    some basic changes in polls

--- first_try/first_try/settings.py
+++ first_try/first_try/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # thried party apps
+
+    #own apps
+    'polls',    
 ]
 
 MIDDLEWARE = [
